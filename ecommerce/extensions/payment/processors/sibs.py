9061956792697ac30a6a10c63f644c2af2a92710
David Moreno <david.moreno@edunext.co>
2020-02-12 17:06:03 -0500

    Get SIBS payment status

--- ecommerce/extensions/payment/processors/sibs.py
+++ ecommerce/extensions/payment/processors/sibs.py
@@ -1,13 +1,12 @@
 """ SIBS payment processing """
-
-
 import logging
-import requests
+
+from decimal import Decimal
 import json
+import requests
 
 from django.urls import reverse
-
-
+from oscar.apps.payment.exceptions import TransactionDeclined
 
 from ecommerce.extensions.payment.processors import BasePaymentProcessor, HandledProcessorResponse
 
@@ -23,6 +22,8 @@ class SIBS(BasePaymentProcessor):
     """
 
     NAME = 'sibs'
+    TRANSACTION_ACCEPTED = '000.100.112'
+    VALID_TEST_MODE = 'EXTERNAL'
 
     def __init__(self, site):
         """
@@ -34,7 +35,7 @@ class SIBS(BasePaymentProcessor):
         self.bearer = configuration['bearer']
         self.entity_id = configuration['entity_id']
         self.test_mode = configuration['test_mode']
-        self.shopperResultUrl = configuration['shopperResultUrl']
+        self.api_root_url = configuration['api_root_url']
 
     def get_transaction_parameters(self, basket, request=None, use_client_side_checkout=False, **kwargs):
         """
@@ -44,19 +45,49 @@ class SIBS(BasePaymentProcessor):
             'amount': str(basket.total_incl_tax),
             'currency': basket.currency,
             'paymentType': 'DB',
-            'entityId': self.entity_id
+            'entityId': self.entity_id,
+            'customParameters[orderNumber]': basket.order_number
         }
+
+        if self.test_mode:
+            parameters['testMode'] = self.VALID_TEST_MODE
+            parameters['customParameters[SIBS_ENV]'] = 'QLY'
+
         response = self._prepare_checkout(parameters)
         parameters['payment_page_url'] = reverse('sibs:payment')
-        parameters['shopperResultUrl'] = self.shopperResultUrl
-        parameters['checkoutId'] = response['id']
+        parameters['shopperResultUrl'] = reverse('sibs:execute')
+        parameters['checkoutId'] = response.get('id')
         return parameters
 
     def handle_processor_response(self, response, basket=None):
         """
-        Execute an approved SIBS payment
+        Handle a SIBS payment status.
+
+        Arguments:
+            response (dict): Dictionary of parameters received from the payment processor.
+
+        Keyword Arguments:
+            basket (Basket): Basket being purchased via the payment processor
+
+        Raises:
+            TransactionDeclined: Indicates the payment was declined by the processor.
         """
-        raise NotImplementedError
+        payment_code_status = response.get('result', {}).get('code')
+        if payment_code_status != self.TRANSACTION_ACCEPTED:
+            raise TransactionDeclined
+
+        transaction_id = response.get('id')
+        total = basket.total_incl_tax
+        currency = basket.currency
+        card_number = response.get('card', {}).get('last4Digits')
+        card_type = response.get('paymentBrand')
+        return HandledProcessorResponse(
+            transaction_id=transaction_id,
+            total=total,
+            currency=currency,
+            card_number=card_number,
+            card_type=card_type
+        )
 
     def issue_credit(self, order_number, basket, reference_number, amount, currency):
         raise NotImplementedError
@@ -64,9 +95,28 @@ class SIBS(BasePaymentProcessor):
 
     def _prepare_checkout(self, data):
         """
-        Implement checkout, get id
+        Perform a server-to-server POST request to prepare the checkout with the required data.
         """
-        response = requests.post('https://test.oppwa.com/v1/checkouts',
-                                 headers={'Authorization': self.bearer},
-                                 data=data).content
+        try:
+            response = requests.post('{0}/v1/checkouts'.format(self.api_root_url),
+                                     headers={'Authorization': self.bearer},
+                                     data=data).content
+        except requests.exceptions.RequestException as err:
+            logger.error('Error preparing checkout [%s]', str(err))
+        return json.loads(response)
+
+    def get_payment_status(self, payment_url):
+        """
+        Obtain the payment status. No need to verify multiple times.
+        """
+        try:
+            url = {
+                'api_root_url': self.api_root_url,
+                'payment_url': payment_url,
+                'entity_id': self.entity_id
+            }
+            response = requests.get('{api_root_url}{payment_url}?entityId={entity_id}'.format(**url),
+                                    headers={'Authorization': self.bearer}).content
+        except requests.exceptions.RequestException as err:
+            logger.error('Error preparing checkout [%s]', str(err))
         return json.loads(response)
--- ecommerce/extensions/payment/views/sibs.py
+++ ecommerce/extensions/payment/views/sibs.py
@@ -1,20 +1,20 @@
 """SIBS payment processing views."""
 import logging
+import json
 
 logger = logging.getLogger(__name__)
 
-
+from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db import transaction
-from django.http import Http404, HttpResponse, HttpResponseBadRequest
 from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
-from oscar.apps.payment.exceptions import PaymentError
+from oscar.apps.partner import strategy
+from oscar.apps.payment.exceptions import TransactionDeclined
 from oscar.core.loading import get_class, get_model
 
-from ecommerce.extensions.basket.utils import basket_add_organization_attribute
 from ecommerce.extensions.checkout.mixins import EdxOrderPlacementMixin
 from ecommerce.extensions.checkout.utils import get_receipt_page_url
 from ecommerce.extensions.payment.processors.sibs import SIBS
@@ -23,8 +23,6 @@ logger = logging.getLogger(__name__)
 
 Applicator = get_class('offer.applicator', 'Applicator')
 Basket = get_model('basket', 'Basket')
-BillingAddress = get_model('order', 'BillingAddress')
-Country = get_model('address', 'Country')
 NoShippingRequired = get_class('shipping.methods', 'NoShippingRequired')
 OrderNumberGenerator = get_class('order.utils', 'OrderNumberGenerator')
 OrderTotalCalculator = get_class('checkout.calculators', 'OrderTotalCalculator')
@@ -40,19 +38,16 @@ class SIBSView(LoginRequiredMixin, View):
     def dispatch(self, request, *args, **kwargs):
         return super(SIBSView, self).dispatch(request, *args, **kwargs)
 
-    def get(self, request):
-        raise NotImplementedError
-
     def post(self, request):
+        """Create the payment form."""
         try:
             context = {
                 'shopperResultUrl': request.POST['shopperResultUrl'],
                 'checkoutId': request.POST['checkoutId']
             }
-            return render(request, 'payment/sibs.html', context)
         except Exception as err: # pylint: disable=broad-except
             logger.error('SIBS failed with error [%s]', str(err))
-        raise NotImplementedError
+        return render(request, 'payment/sibs.html', context)
 
 
 class SIBSPaymentExecutionView(EdxOrderPlacementMixin, View):
@@ -62,13 +57,74 @@ class SIBSPaymentExecutionView(EdxOrderPlacementMixin, View):
     def payment_processor(self):
         return SIBS(self.request.site)
 
-    def _get_basket(self, payment_id):
-        raise NotImplementedError
+    def _get_basket(self, basket_id):
+        """Return basket object from the basket_id."""
+        if not basket_id:
+            return None
+
+        try:
+            basket_id = int(basket_id)
+            basket = Basket.objects.get(id=basket_id)
+            basket.strategy = strategy.Default()
+            Applicator().apply(basket, basket.owner, self.request)
+            return basket
+        except (ValueError, ObjectDoesNotExist):
+            return None
+
 
     def get(self, request):
-        """Handle an incoming user returned to us by SIBS, (sibs UI)"""
-        raise NotImplementedError
+        """Handle the order and SIBS status."""
+        sibs_status = self.payment_processor.get_payment_status(request.GET['resourcePath'])
+        basket = None
 
-    def post(self, request):
-        """post"""
-        raise NotImplementedError
+        try:
+            transaction_id = sibs_status.get('id')
+            order_number = sibs_status.get('customParameters', {}).get('orderNumber')
+            basket_id = OrderNumberGenerator().basket_id(order_number)
+            basket = self._get_basket(basket_id)
+            logger.info(
+                'Received SIBS payment status for transaction [%s], associated with basket [%d].',
+                transaction_id,
+                basket_id
+            )
+        finally:
+            ppr = self.payment_processor.record_processor_response(
+                json.dumps(sibs_status),
+                transaction_id=transaction_id,
+                basket=basket
+            )
+
+        try:
+            self.handle_payment(sibs_status, basket)
+            receipt_url = get_receipt_page_url(
+                order_number=order_number,
+                site_configuration=basket.site.siteconfiguration
+            )
+        except TransactionDeclined:
+            logger.exception(
+                'Received an invalid SIBS response. The payment response [%d].',
+                ppr.id
+            )
+            return redirect(reverse('payment_error'))
+
+        try:
+            shipping_method = NoShippingRequired()
+            shipping_charge = shipping_method.calculate(basket)
+            user = basket.owner
+            order_total = OrderTotalCalculator().calculate(basket, shipping_charge)
+            billing_address = None
+            self.handle_order_placement(
+                order_number,
+                user,
+                basket,
+                None,
+                shipping_method,
+                shipping_charge,
+                billing_address,
+                order_total,
+                request=request
+            )
+        except: # pylint: disable=bare-except
+            payment_processor = self.payment_processor.NAME.title() if self.payment_processor else None
+            logger.exception(self.order_placement_failure_msg, payment_processor, basket.id)
+        return redirect(receipt_url)
